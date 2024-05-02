0db92932fe46dbe57ce9101700659b381fc69a22
Bintang Anandhiya <bintanganandhiya23@gmail.com>
2024-05-02 15:39:16 +0700

    Push

--- CineChoice/Views/FilmView.swift
+++ CineChoice/Views/FilmView.swift
@@ -53,7 +53,7 @@ struct GridItemDemo2: View {
                     
                 ForEach(0...22, id: \.self) { x in
                     
-                    KFImage(URL(string: "https://v5.airtableusercontent.com/v3/u/28/28/1714399200000/pY_6TOtWGxIMgAbZF35Bgg/KFSt03snBSNIEqJOlVnxJx2QoMVjyw3ezA1xdQVtRQ_G3zHn1fkrFI6myoEH6eIMJS9y-wtG1LOBiQX4q58D59UhNAkGHTU8BaPhF8Y0oVOJ5irm7jnrDLgWlJgOkL2hPD6gsc9VyORLpBsIk62kRF1zueL4CMiT_eirNYeb65E/A5P5lQS0F6r3vcZ6b4xJ6ynKSecFieRFNR5LNCwMj98")!)
+                    KFImage(URL(string: "https://shucboqluxegybsrzyfz.supabase.co/storage/v1/object/sign/filmPosters/avengers%20endgame.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJmaWxtUG9zdGVycy9hdmVuZ2VycyBlbmRnYW1lLnBuZyIsImlhdCI6MTcxNDQ0OTg5MSwiZXhwIjoxNzQ1OTg1ODkxfQ.RSHkioYcUaAvEtpwSDRcw1saAlXsLNhRo6Q6QT9cz38&t=2024-04-30T04%3A04%3A51.960Z")!)
                         .resizable()
                         .scaledToFill()
                         .frame(width: 3*gs,height: 3*gs)
@@ -75,67 +75,107 @@ struct GridItemDemo2: View {
 }
 
 struct FilmView: View {
-
-    private var gs = UIScreen.main.bounds.width/17
+    
+    var selectedFilm: Int
+    
+    @EnvironmentObject var supabaseManager: SupabaseManager
+    @Environment(\.presentationMode) var presentationMode
+    
+    private let gs = UIScreen.main.bounds.width/17
+    
+    init(selectedFilm: Int) {
+        self.selectedFilm = selectedFilm
+        print("WOIII: " + String(self.selectedFilm))
+    }
     
     var body: some View {
+        
         ScrollView {
+            
+            Button(action: {
+                self.presentationMode.wrappedValue.dismiss()
+            }) {
+                Image(systemName: "xmark")
+                    .foregroundColor(.white)
+                    .font(.title2)
+            }
+            
             VStack(spacing: gs) {
                 
                 HStack(spacing: gs){
+                    VStack{
+                        ForEach(supabaseManager.films.indices, id: \.self){ index in
+                            Text(String(supabaseManager.films[index].filmID)).font(.system(size: 5)).foregroundStyle(.red)
+                            Text(String(selectedFilm)).font(.system(size: 5)).foregroundStyle(.red)
+                            if (supabaseManager.films[index].filmID == 12){
+                                KFImage(URL(string: supabaseManager.films[index].filmPoster)!)
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: gs*7,height: gs*11)
+                                    .clipShape(RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/))
+                                
+                            }
+                        }
+                    }
                     
-                    Image("c01")
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: gs*7,height: gs*11)
-                        .clipShape(RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/))
 
                     VStack(spacing:gs){
-                        HStack{
-                            
-                            Image(systemName: "hand.thumbsup.circle.fill")
-                                .resizable()
-                                .foregroundColor(.yellow)
-                                .scaledToFill()
-                                .frame(width: gs)
-                            Spacer()
-                            Text("50%")
-                                .font(.system(size: 25))
-                                .foregroundStyle(.white)
+                        ForEach(supabaseManager.filmRatings.indices, id: \.self){ index in
+                            if (supabaseManager.filmRatings[index].filmID == selectedFilm){
+                                
+                                
+                                HStack{
+                                    
+                                    Image(systemName: "hand.thumbsup.circle.fill")
+                                        .resizable()
+                                        .foregroundColor(.yellow)
+                                        .scaledToFill()
+                                        .frame(width: gs)
+                                    Spacer()
+                                    
+                                    Text(String(supabaseManager.filmRatings[index].averageLike)+"%")
+                                        .font(.system(size: 25))
+                                        .foregroundStyle(.white)
+                                    
+                                    
+                                    
+                                }
+                                .padding(20)
+                                .frame(width: gs*7, height:gs*3)
+                                .background(RoundedRectangle(cornerRadius: 10).fill(Color("CCGray2")))
+                                
+                                HStack{
+                                    Image(systemName: "hand.thumbsdown.circle.fill")
+                                        .resizable()
+                                        .foregroundColor(.red)
+                                        .scaledToFill()
+                                        .frame(width: gs)
+                                    Spacer()
+                                    Text(String(supabaseManager.filmRatings[index].averageDislike)+"%")
+                                        .font(.system(size: 25))
+                                        .foregroundStyle(.white)
+                                }
+                                .padding(20)
+                                .frame(width: gs*7, height:gs*3)
+                                .background(RoundedRectangle(cornerRadius: 10).fill(Color("CCGray2")))
+                                
+                                HStack{
+                                    Image(systemName: "eye.slash.circle.fill")
+                                        .resizable()
+                                        .foregroundColor(Color(.lightGray))
+                                        .scaledToFill()
+                                        .frame(width: gs)
+                                    Spacer()
+                                    Text(String(supabaseManager.filmRatings[index].averageUnseen)+"%")
+                                        .font(.system(size: 25))
+                                        .foregroundStyle(.white)
+                                }
+                                .padding(20)
+                                .frame(width: gs*7, height:gs*3)
+                                .background(RoundedRectangle(cornerRadius: 10).fill(Color("CCGray2")))
+                            }
                         }
-                        .padding(20)
-                        .frame(width: gs*7, height:gs*3)
-                        .background(RoundedRectangle(cornerRadius: 10).fill(Color("CCGray2")))
                         
-                        HStack{
-                            Image(systemName: "hand.thumbsdown.circle.fill")
-                                .resizable()
-                                .foregroundColor(.red)
-                                .scaledToFill()
-                                .frame(width: gs)
-                            Spacer()
-                            Text("33%")
-                                .font(.system(size: 25))
-                                .foregroundStyle(.white)
-                        }
-                        .padding(20)
-                        .frame(width: gs*7, height:gs*3)
-                        .background(RoundedRectangle(cornerRadius: 10).fill(Color("CCGray2")))
-                        
-                        HStack{
-                            Image(systemName: "eye.slash.circle.fill")
-                                .resizable()
-                                .foregroundColor(Color(.lightGray))
-                                .scaledToFill()
-                                .frame(width: gs)
-                            Spacer()
-                            Text("12%")
-                                .font(.system(size: 25))
-                                .foregroundStyle(.white)
-                        }
-                        .padding(20)
-                        .frame(width: gs*7, height:gs*3)
-                        .background(RoundedRectangle(cornerRadius: 10).fill(Color("CCGray2")))
                         
                         
                     }
@@ -156,6 +196,7 @@ struct FilmView: View {
             }
             .frame(minWidth:17*gs,minHeight: 700)
         }
+        .navigationBarBackButtonHidden(true)
         .background(Color("CCGray1"))
         
         
@@ -163,6 +204,6 @@ struct FilmView: View {
     }
 }
 
-#Preview {
-    FilmView()
-}
+//#Preview {
+//    FilmView()
+//}
